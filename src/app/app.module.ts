51afb2d44732becd3eacb9634aa50c0478e7538e
dannnnnh <113985769+dannnnnh@users.noreply.github.com>
2023-05-25 16:20:20 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,48 @@
-import {
-    NgModule
-}
-from '@angular/core';
-import {
-    BrowserModule
-}
-from '@angular/platform-browser';
-import {
-    HttpClientModule
-}
-from '@angular/common/http';
-import {
-    AppRoutingModule
-}
-from './app-routing.module';
-import {
-    AppComponent
-}
-from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { MatInputModule } from "@angular/material/input";   
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDialogModule } from "@angular/material/dialog";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { UserRegistrationFormComponent } from "./user-registration-form/user-registration-form.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
-@NgModule({ declarations: [AppComponent ], imports: [BrowserModule, AppRoutingModule ], providers: [], bootstrap: [AppComponent] }) export class AppModule {}
+
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        UserRegistrationFormComponent,
+        UserLoginFormComponent,
+
+        
+        
+    ],
+    imports: [
+        MatCardModule,
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatSnackBarModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatDialogModule,
+        MatCardModule
+    ],
+
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
